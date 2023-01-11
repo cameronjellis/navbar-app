314a18911189ea337e1e70c90a43970e6c4bbeae
cameronjellis <cameron.ellis@siliconmtn.com>
2023-01-10 17:15:15 -0700

    bootstrap addition tabs for nav

--- src/root.component.js
+++ src/root.component.js
@@ -1,8 +1,25 @@
-export default function Root(props) {
+import { useState } from "react";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+
+const Root = (props) => {
+  const [key, setKey] = useState("home");
+
   return (
     <div>
       <h1>NavBAR</h1>
       <section>{props.name} is mounted!</section>
+      <Tabs
+        id="controlled-tab-example"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+        className="mb-3"
+      >
+        <Tab eventKey="home" title="Home"></Tab>
+        <Tab eventKey="graph" title="Graph"></Tab>
+      </Tabs>
     </div>
   );
-}
+};
+
+export default Root;
