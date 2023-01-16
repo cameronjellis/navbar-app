5048a0b6b49f201949f45c405c2f30e397ec1fc5
cameronjellis <cameron.ellis@siliconmtn.com>
2023-01-16 11:25:48 -0700

    ...

--- src/root.component.js
+++ src/root.component.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import Tab from "react-bootstrap/Tab";
-import Tabs from "react-bootstrap/Tabs";
 import "bootstrap/dist/css/bootstrap.min.css";
+import DataNavBar from "./Components/DataNavBar";
 
 const Root = (props) => {
   const [key, setKey] = useState("home");
@@ -10,15 +9,7 @@ const Root = (props) => {
     <div>
       <h1>NavBAR</h1>
       <section>{props.name} is mounted!</section>
-      <Tabs
-        id="controlled-tab-example"
-        activeKey={key}
-        onSelect={(k) => setKey(k)}
-        className="mb-3"
-      >
-        <Tab eventKey="home" title="Home"></Tab>
-        <Tab eventKey="graph" title="Graph"></Tab>
-      </Tabs>
+      <DataNavBar />
     </div>
   );
 };
