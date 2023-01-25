899d92938fcb78431db7989e20f077443244ab9d
cameronjellis <cameron.ellis@siliconmtn.com>
2023-01-25 15:24:45 -0700

    nav bar update

--- src/Components/DataNavBar.jsx
+++ src/Components/DataNavBar.jsx
@@ -1,24 +1,38 @@
-import React from "react";
+import { React, useState } from "react";
 import { Nav } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
-class DataNavBar extends React.Component {
-  constructor(...args) {
-    super(...args);
-    this.state = { data: null };
-  }
+const DataNavBar = () => {
+  const [page, setPage] = useState("home");
 
-  render() {
-    return (
-      <Nav defaultActiveKey="/home" className="flex-column">
-        <Nav.Link href="/home">Graph</Nav.Link>
-        <Nav.Link eventKey="link-1">Schema</Nav.Link>
-        <Nav.Link eventKey="link-2">Table</Nav.Link>
-        <Nav.Link eventKey="disabled" disabled>
-          Disabled
-        </Nav.Link>
-      </Nav>
-    );
-  }
-}
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/home">~ N G A ~</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/graph">Graph</Nav.Link>
+            <Nav.Link href="/micromanager">Microservice Manager</Nav.Link>
+            <NavDropdown title="options" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default DataNavBar;
--- src/root.component.js
+++ src/root.component.js
@@ -5,13 +5,7 @@ import DataNavBar from "./Components/DataNavBar";
 const Root = (props) => {
   const [key, setKey] = useState("home");
 
-  return (
-    <div>
-      <h1>NavBAR</h1>
-      <section>{props.name} is mounted!</section>
-      <DataNavBar />
-    </div>
-  );
+  return <DataNavBar props={props} />;
 };
 
 export default Root;
